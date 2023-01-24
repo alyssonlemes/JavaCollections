7a3e451bba0c1eadc31dc792f613b56531007035
Alysson Lemes <65565603+alyssonlemes@users.noreply.github.com>
2023-01-23 22:06:13 -0300

    Update TestaCurso2.java

--- bin/br/com/alura/TestaCurso2.java
+++ bin/br/com/alura/TestaCurso2.java
@@ -1,32 +1,82 @@
-package br.com.alura;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class TestaCurso2 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Curso javaColecoes = new Curso("Dominando as coleções do java", "Paulo Silveira");
-		
+        Curso javaColecoes = new Curso("Dominando as colecoes do Java",
+                "Paulo Silveira");
 
-		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
-		javaColecoes.adiciona(new Aula("Criando uma aula", 20));
-		javaColecoes.adiciona(new Aula("Modelando com coleções", 22));
-		
-		
-		List<Aula> aulasImutaveis = javaColecoes.getAulas();
-		System.out.println(aulasImutaveis);
-		
-		List<Aula> aulas = new ArrayList<>(aulasImutaveis);
-		
-		Collections.sort(aulas);
-		System.out.println(aulas);
-		
-		System.out.println(aulas);
-		System.out.println(javaColecoes.getTempoTotal());
-		
-	}
+        javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
+        javaColecoes.adiciona(new Aula("Criando uma Aula", 20));
+        javaColecoes.adiciona(new Aula("Modelando com colecoes", 24));
 
+        List<Aula> aulas = javaColecoes.getAulas();
+        System.out.println(aulas);
+
+        Collections.sort(aulas);
+        System.out.println(aulas);
+
+    }
+}
+
+public class TestaCurso2 {
+
+    private String nome;
+    private String instrutor;
+    private List<Aula> aulas = new LinkedList<Aula>();
+
+    public Curso(String nome, String instrutor) {
+        this.nome = nome;
+        this.instrutor = instrutor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getInstrutor() {
+        return instrutor;
+    }
+
+    public List<Aula> getAulas() {
+        return Collections.unmodifiableList(aulas);
+    }
+
+    public void adiciona(Aula aula) {
+        this.aulas.add(aula);
+    }
+
+    public int getTempoTotal() {
+        return this.aulas.stream().mapToInt(Aula::getTempo).sum();
+    }
+}
+
+public class Aula implements Comparable<Aula> {
+
+    private String titulo;
+    private int tempo;
+
+    public Aula(String titulo, int tempo) {
+        this.titulo = titulo;
+        this.tempo = tempo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getTempo() {
+        return tempo;
+    }
+
+    @Override
+    public String toString() {
+        return "[Aula: " + this.titulo + ", " + this.tempo + " minutos]";
+    }
+
+    @Override
+    public int compareTo(Aula outraAula) {
+        return this.titulo.compareTo(outraAula.getTitulo());
+    }
 }
