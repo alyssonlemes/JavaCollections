8e22e51004e2ac5499c439359c54f8d07961bd42
Alysson Lemes <65565603+alyssonlemes@users.noreply.github.com>
2023-01-24 20:58:16 -0300

    Update TestaCurso2.java

--- bin/br/com/alura/TestaCurso2.java
+++ bin/br/com/alura/TestaCurso2.java
@@ -1,4 +1,8 @@
-import java.util.*;
+package br.com.alura;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class TestaCurso2 {
 
@@ -11,72 +15,14 @@ public class TestaCurso2 {
         javaColecoes.adiciona(new Aula("Criando uma Aula", 20));
         javaColecoes.adiciona(new Aula("Modelando com colecoes", 24));
 
-        List<Aula> aulas = javaColecoes.getAulas();
-        System.out.println(aulas);
+        List<Aula> aulasImutaveis = javaColecoes.getAulas();
+        System.out.println(aulasImutaveis);
+        
+        List<Aula> aulas = new ArrayList<>(aulasImutaveis);
+        
+        Collections.sort(aulasImutaveis);
+        System.out.println(aulasImutaveis);
 
-        Collections.sort(aulas);
-        System.out.println(aulas);
 
-    }
-}
-
-public class TestaCurso2 {
-
-    private String nome;
-    private String instrutor;
-    private List<Aula> aulas = new LinkedList<Aula>();
-
-    public Curso(String nome, String instrutor) {
-        this.nome = nome;
-        this.instrutor = instrutor;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getInstrutor() {
-        return instrutor;
-    }
-
-    public List<Aula> getAulas() {
-        return Collections.unmodifiableList(aulas);
-    }
-
-    public void adiciona(Aula aula) {
-        this.aulas.add(aula);
-    }
-
-    public int getTempoTotal() {
-        return this.aulas.stream().mapToInt(Aula::getTempo).sum();
-    }
-}
-
-public class Aula implements Comparable<Aula> {
-
-    private String titulo;
-    private int tempo;
-
-    public Aula(String titulo, int tempo) {
-        this.titulo = titulo;
-        this.tempo = tempo;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public int getTempo() {
-        return tempo;
-    }
-
-    @Override
-    public String toString() {
-        return "[Aula: " + this.titulo + ", " + this.tempo + " minutos]";
-    }
-
-    @Override
-    public int compareTo(Aula outraAula) {
-        return this.titulo.compareTo(outraAula.getTitulo());
     }
 }
