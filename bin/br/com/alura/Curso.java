06a08f076e817f4fd203210a9e910fd3ea58bfb2
Alysson Lemes <65565603+alyssonlemes@users.noreply.github.com>
2023-01-25 16:47:26 -0300

    Update Curso.java

--- bin/br/com/alura/Curso.java
+++ bin/br/com/alura/Curso.java
@@ -1,17 +1,16 @@
 package br.com.alura;
 
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Curso {
 
 	private String nome;
 	private String instrutor;
-	private List<Aula> aulas = new ArrayList<Aula>();
+	private List<Aula> aulas = new LinkedList<Aula>();
 
 	public Curso(String nome, String instrutor) {
-		super();
 		this.nome = nome;
 		this.instrutor = instrutor;
 	}
@@ -27,9 +26,19 @@ public class Curso {
 	public List<Aula> getAulas() {
 		return Collections.unmodifiableList(aulas);
 	}
-	
+
 	public void adiciona(Aula aula) {
 		this.aulas.add(aula);
+	}
+
+	public int getTempoTotal() {
+		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
+	}
+
+	@Override
+	public String toString() {
+		return "[Curso: " + nome + ", tempo total: " + this.getTempoTotal()
+				+ ", aulas: + " + this.aulas + "]";
 	}
 
 }
